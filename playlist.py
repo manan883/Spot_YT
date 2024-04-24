8fdc62cdec3bdb4a843cce990efd5e815a762eff
manan883 <manan883@gmail.com>
2024-04-23 23:24:52 -0700

    fixes

--- playlist.py
+++ playlist.py
@@ -4,6 +4,8 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import os
+from dotenv import load_dotenv, dotenv_values
+load_dotenv()
 SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
 SPOTIFY_REDIRECT_URI = os.environ.get('SPOTIFY_REDIRECT_URI')
